1b05cdaa2a229e4317d760128e9f31125a2c4486
Rick Bacci <rickbacci@gmail.com>
2016-01-13 19:39:17 -0700

    setup basic express configuration

---
+++ server.js
@@ -0,0 +1,17 @@
+const http  = require('http');
+var express = require('express');
+var app     = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+})
+
+
+
+module.exports = app;
