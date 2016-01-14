2bed8c59e0be539d0d3ced39cd9fa8b3d62f366c
Rick Bacci <rickbacci@gmail.com>
2016-01-13 20:53:39 -0700

    print poll to console

---
+++ public/client.js
@@ -0,0 +1,24 @@
+var submitPoll = document.getElementById('submit-poll');
+
+submitPoll.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  var question = document.getElementById('question').value;
+  var choiceA  = document.getElementById('choice-A').value;
+  var choiceB  = document.getElementById('choice-B').value;
+  var choiceC  = document.getElementById('choice-C').value;
+  var choiceD  = document.getElementById('choice-D').value;
+
+  var poll = {
+    question: question,
+    choiceA: choiceA,
+    choiceB: choiceB,
+    choiceC: choiceC,
+    choiceD: choiceD,
+  }
+  console.log(poll);
+});
+
+
+
+
