b6069051ff01b70201ce324892ad381b85753eed
Rick Bacci <rickbacci@gmail.com>
2016-01-14 00:03:11 -0700

    add random url creation
    When a poll is created an object containing the
    poll data is sent to the server. Socket.io picks
    up the message and adds it to the polls hash.
    
    The random urls are then sent back to the client, 
    and displayed on the page.

--- public/client.js
+++ public/client.js
@@ -19,8 +19,17 @@ submitPoll.addEventListener('click', function(event) {
     choiceD: choiceD,
   }
   console.log(poll);
+  socket.send('pollCreated', poll)
 });
 
 
+var adminUrl = document.getElementById('admin-url');
+var voterUrl = document.getElementById('voter-url');
+var baseUrl  = 'http://localhost:3000'
+
+socket.on('urls', function(urls) {
+  adminUrl.innerText = "Admin Url: " + baseUrl + urls.admin;
+  voterUrl.innerText = "Voter Url: " + baseUrl + urls.voter;
+});
 
 
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const http  = require('http');
 var express = require('express');
 var app     = express();
+var crypto  = require('crypto');
+
+var polls   = {};
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res){
-  res.sendFile(__dirname + '/public/index.html');
+app.get('/', function (request, response){
+  response.sendFile(__dirname + '/public/index.html');
 });
 
 var port   = process.env.PORT || 3000;
-
 var server = http.createServer(app).listen(port, function () {
   console.log('Listening on port ' + port + '.');
 });
@@ -17,6 +19,26 @@ var server = http.createServer(app).listen(port, function () {
 const socketIo = require('socket.io');
 const io       = socketIo(server);
 
+io.on('connection', function(socket) {
+
+  socket.on('message', function (channel, message) {
+    var adminString = 'poll-'  + randomString();
+    var voterString = 'voter-' + randomString();
+
+    if (channel === 'pollCreated') {
+      polls[adminString] = {
+        voterString: voterString,
+        poll: message
+      }
+      socket.emit('urls', { admin: adminString, voter: voterString });
+
+    }
+  });
+});
+
+function randomString() {
+  return crypto.randomBytes(10).toString('hex');
+}
 
 
 module.exports = server;
