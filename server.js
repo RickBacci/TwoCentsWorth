fbd89d41e50e1dfb347ba0cc41f1d3dc7c5124d7
Rick Bacci <rickbacci@gmail.com>
2016-01-18 11:24:19 -0700

    remove Redis

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ var express      = require('express');
 var app          = express();
 var randomString = require('./random-string');
 var bodyParser   = require('body-parser');
-var redis        = configureRedis();
 var Poll         = require('./poll');
 
+var polls        = [];
 
-redis.on('connect', function() { console.log('Redis server connected'); });
-redis.on("error", function (err) { console.log("Error " + err); });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -26,8 +24,6 @@ app.post('/polls', function(request, response) {
   var voterString = randomString();
   var id          = adminString + voterString;
   var poll        = new Poll(id, adminString, voterString, request);
-
-  storePoll(id, poll, redis);
 
   response.render('poll', { poll: poll });
 });
@@ -47,71 +43,17 @@ app.get('/polls/:id', function(request, response) {
 
   var poll;
 
-  redis.hgetall("polls", function (err, poll) {
-
-    formatData(poll, buildHostString(request));
-
     response.render('poll', { poll: poll })
 
   });
 
 });
 
-function buildHostString(request) {
-  return request.protocol + '://' + request.get('host') + "/polls/"
-}
-
-
-function formatData(poll, host) {
-  poll.adminUrl = host + poll.adminString
-  poll.voterUrl = host + poll.voterString
-  poll.choices = poll.choices.split(',')
-
-  return poll;
-}
-
 
 var port   = process.env.PORT || 3000;
 var server = http.createServer(app).listen(port, function () {
   console.log('Listening on port ' + port + '.');
 });
-
-
-
-function configureRedis() {
-  if (process.env.REDISTOGO_URL) {
-    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-    var redis = require("redis").createClient(rtg.port, rtg.hostname);
-    redis.auth(rtg.auth.split(":")[1]);
-    return redis;
-  } else {
-    var redis = require("redis").createClient();
-    return redis;
-  }
-}
-
-function logRedisKeys(redis) {
-  redis.hkeys('polls', function (err, keys) {
-    keys.forEach(function (key) {
-      redis.hget(
-        'polls', key,
-        function(err, value) {
-          console.log(key + ': ' + value);
-        });
-    });
-  });
-}
-
-function storePoll(poll_id, poll, redis) {
-  redis.hmset(poll_id, poll)
-}
-
-
-// TODO move Poll to it's own file
-
-  this.voterUrl    = '/polls/' + voterString;
-}
-
 
 
 module.exports = server;
