540573e3722d67ba4527c1750bbc7b2bc8246534
Rick Bacci <rickbacci@gmail.com>
2016-01-27 11:58:50 -0700

    add generic unit test

---
+++ tests/unit/poll.js
@@ -0,0 +1,11 @@
+var expect = require('expect.js');
+
+describe('Poll', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      expect([1,2,3].indexOf(0)).to.eql(-1)
+      expect([1,2,3].indexOf(5)).to.eql(-1)
+    });
+  });
+});
+
